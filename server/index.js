872654bc319cdf1b7a133c3187e07770dfd776ab
Monika Gapińska <monika.gapinska@lppsa.com>
2021-04-18 21:34:10 +0200

    server update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const http = require('http')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const app = express()
-const router = require('./router')
-const mongoose = require('mongoose')
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-//DB Setup
-mongoose.connect('mongodb://localhost/auth')
+// DB Setup
+mongoose.connect('mongodb://localhost/auth');
 
-app.use(morgan('combined'))
-app.use(bodyParser.json({ type: '*/*' }))
-router(app)
+// App Setup
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
-const port = process.env.PORT || 3090
-const server = http.createServer(app)
-server.listen(port)
-console.log('Server listening on: ', port)
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
