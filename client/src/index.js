1292037e61fcf82de3dce5f366bb04ad2cba4eb2
Monika Gapińska <monika.gapinska@lppsa.com>
2021-04-19 16:16:51 +0200

    client init

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Welcome from './components/Welcome'
+import App from './components/App'
+
+ReactDOM.render(
+	<BrowserRouter>
+		<App>
+			<Route path="/" exact component={Welcome} />
+		</App>
+	</BrowserRouter>,
+	document.querySelector('#root')
+)
