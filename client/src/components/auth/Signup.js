379b231fdf3cf8e34df22185023a8025e651cb1f
gapinska <gapinska.mon@gmail.com>
2021-04-27 19:09:42 +0200

    handling form submission

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { reduxForm, Field } from 'redux-form'
+import React from "react"
+import { reduxForm, Field } from "redux-form"
 
-const Signup = () => {
-	return (
-		<form>
-			<fieldset>
-				<label>Email</label>
-				<Field name="email" type="text" component="input" autoComplete="none" />
-			</fieldset>
-			<fieldset>
-				<label>Password</label>
-				<Field name="password" type="password" component="input" autoComplete="none" />
-			</fieldset>
-		</form>
-	)
+const Signup = ({ handleSubmit }) => {
+  const onSubmit = (formProps) => {
+    console.log(formProps)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <label>Email</label>
+        <Field name="email" type="text" component="input" autoComplete="none" />
+      </fieldset>
+      <fieldset>
+        <label>Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <button>Sign up</button>
+    </form>
+  )
 }
 
-export default reduxForm({ form: 'signup' })(Signup)
+export default reduxForm({ form: "signup" })(Signup)
